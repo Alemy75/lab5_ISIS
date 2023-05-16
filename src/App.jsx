b56d5ed3c1c211ecc8fee1c17e332d9ba3cd3f7f
Ilya <ilja02092002@mail.ru>
2023-05-16 13:14:35 +0300

    первый коммит в dev ветку

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [name, setName] = useState('')
+  const [desc, setDesc] = useState('')
+  const [items, setItems] = useState([])
+
+
+  const onSumbitHandler = (event) => {
+    event.preventDefault()
+    let item = {
+      name,
+      desc
+    }
+    setItems(prev => {
+      return [...prev, item]
+    })
+    setName('')
+    setDesc('')
+  }
+
+  return (
+    <div className='container'>
+      <h1>Приложение заметок</h1>
+      <form onSubmit={onSumbitHandler}>
+        <input value={name} onChange={(event) => setName(event.target.value)} type="text" placeholder='Введите название' />
+        <input value={desc} onChange={(event) => setDesc(event.target.value)} type="text" placeholder='Введите описание' />
+        <button>Добавить запись</button>
+      </form>
+      <h2>Заметки</h2>
+      {
+        items.map(item => (
+          <div className='card'>
+            <h3>{item.name}</h3>
+            <p>{item.desc}</p>
+          </div>
+        ))
+      }
+    </div>
+  )
+}
+
+export default App
